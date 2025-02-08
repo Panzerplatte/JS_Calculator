c2a8c3685307d1080b31f80f5e38ae13758d7350
Panzerplatte <Thenerver01@gmail.com>
2025-02-08 16:55:13 +0100

    Bug Fixing
    some bugs got fixed and the logic optimized

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const display = document.getElementById("display") // Hier weißen wir das Display dem script hinzu um die Zahlen aus diesem entnehmen zu können.
 
 var lastDisplay = [] // Array um alle aktuellen Zahlen und Operator zu speichern.
+var lastCharacter = "0" // Dies ist immer der Aktuelle letzte Char den wir eingegeben haben.
 
-// TODO \\
-// Ich möchte nicht das Opperatiren doppelt ausgewählt werden können.
 
 // ----- Essentials ----- \\
 
 // Hier schauen wir nach ob der letzte Character eine Zahl oder ein Operator ist.
-function checkLastCharacter() {
-
-    const str = lastDisplay[lastDisplay.length - 1]
+function checkLastCharacter(str) {
 
     if (/\d$/.test(str)) {
         return str
@@ -22,9 +19,17 @@ function checkLastCharacter() {
 
 }
 
-// Hier ersetzten wir die Letzte Position mit einer Zahl.
+// Hier ersetzten wir die Letzte Position mit einem anderen Char. \\ Wir erhalten einen String zurück
 function replaceLastCharacter(str, newNumber) {
-    return str.replace(/\d$/, newNumber)
+
+    var isNumber = checkLastCharacter(lastCharacter)
+
+    if (isNumber == false) {
+        return str.replace(/[+\-*/]$/, newNumber)
+    }
+    else {
+        return str.replace(/\d$/, newNumber)
+    }
 }
 
 // Hier schauen wir nach ob es eine einzelne nummer ist oder ob es eine Calculation ist.
@@ -48,14 +53,54 @@ function PositivOrNegativ(num) {
 
 // Hier fügen wir den Input zu dem Display String und der Tabelle hinzu.
 function ShowToDisplay(input){
-    if (display.value == 0) {
-        display.value = input
-    }
-    else {
-        display.value += input
+    
+    var IsNumber = checkLastCharacter(input) // Hier schauen wir ob der Input eine nummer oder ein Operator ist.
+
+    if (display.value == 0 && input == 0) { // Wenn der Angezeigte wert 0 ist möchten wir keinen weiteren hinzufügen
+        
+        return
+
     }
 
-    lastDisplay.push(input)
+    if (display.value == 0) { // Wenn wir auf dem Display eine 0 Stehen haben wissen wir das wir am anfang einer Calculation sind.
+        
+        if (IsNumber == false) { // Wir wollen überprüfen ob wir es hier um einen Operator zutun haben, wenn ja dann soll dieser zu der 0 hinzugefügt werden
+
+            display.value += input
+
+        }
+
+        else { // Ansonsten ändern wir die 0 zu dem Input da es eine neue Zahl ist.
+            
+            display.value = input
+
+        }
+
+        lastCharacter = input // Wir geben an das der Neue Letzte Character dieser ist den wir gerade angegeben haben.
+
+    }
+
+    else { // Wenn wir auf dem Display keine null zustehen haben müssen wir schauen was genau dort steht bevor wir etwas hinzufügen können.
+        
+        var lastNumber = checkLastCharacter(lastCharacter) // Wir schauen hier nach dem letzten Char der hinzugefügt wurde ob dieser eine Nummer/Char ist oder ein Operator.
+        
+        if (lastNumber == false && IsNumber == false) { // Wenn die Letzte Zahl die wir hinzugefügt haben ein Operator ist und die neue Zahl auch ein Operator ist dann möchten wir diese ersetzten
+            
+            lastCharacter = input
+
+            display.value = replaceLastCharacter(display.value, input)
+
+            return
+
+        }
+        
+        lastCharacter = input
+
+        display.value += input
+
+    }
+
+    lastDisplay.push(input) // wir setzen den Input an die letzte stelle des Arrays
 }
 
 // Hier Setzten wir das Display und alle relevanten sachen zurück.
@@ -88,7 +133,7 @@ function changeNumberValue(){
 
     else {
         
-        var IsAnumber = checkLastCharacter() // hier schauen wir nach ob die letzte zahl eine Zahl ist.
+        var IsAnumber = checkLastCharacter(lastDisplay[lastDisplay.length - 1]) // hier schauen wir nach ob die letzte zahl eine Zahl ist.
         
         if (IsAnumber) {
            
